5b510e01b1aadf3471be32a77dc4e5ec1fbd9abf
AlenaShkr <elalshkr@gmail.com>
2018-09-29 22:42:38 +0300

    zeros:implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  const FIRST=2;
+  const SECOND=5;
+  let resultdevision1=0, resultdevision2=0;
+  let number1=number;
+  let number2=number;
+while (Math.floor(number1/FIRST)!=0){
+  number1=number1/FIRST;
+  resultdevision1+=Math.floor(number1);
+}
+while (Math.floor(number2/SECOND)!=0){
+  number2=number2/SECOND;
+  resultdevision2+=Math.floor(number2);
+ }
+  return Math.min(resultdevision1, resultdevision2);
 }
